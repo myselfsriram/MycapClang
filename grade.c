3018f552d36e99f338da9355644d32b8a9f1821d
myselfsriram <97535682+myselfsriram@users.noreply.github.com>
2022-04-30 19:02:29 +0530

    first

---
+++ grade.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+	int marks;
+	printf("Enter the marks\n");
+	scanf("%d",&marks);
+	if(marks>85 && marks<100)
+		printf("Grade A");
+	else if(marks>70 && marks<84)
+		printf("Grade B");
+	else if(marks>55 && marks<69)
+		printf("Grade C");
+	else if(marks>40 && marks<54)
+		printf("Grade D");
+	else if(marks>40)
+		printf("Grade F");
+}
